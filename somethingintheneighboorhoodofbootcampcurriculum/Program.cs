9bd256cf61a39fc87eeac1b159d181e91a35f0c7
Dan Storey <danstorey901@gmail.com>
2021-12-13 23:54:42 -0500

    Add project files.

---
+++ somethingintheneighboorhoodofbootcampcurriculum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace somethingintheneighboorhoodofbootcampcurriculum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var gitgithub = new Topic();
+            gitgithub.Name = "Git/GitHub";
+            gitgithub.Category = "SCM";
+            gitgithub.Difficulty = 2;
+            gitgithub.Print();
+
+            var sqlserver = new Topic()
+            {
+                Name = "SQL Server",
+                Category = "Database",
+                Difficulty = 3
+            };
+// access modifiers - public, {internal}, prvate, protected     
+
+
+        }
+    }
+}
---
+++ somethingintheneighboorhoodofbootcampcurriculum/Topic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace somethingintheneighboorhoodofbootcampcurriculum
+{
+    class Topic
+    {
+        private int NextId { get; set; } = 13;
+
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Difficulty { get; set; }
+
+        public void Print() {
+            Console.WriteLine($" {Id} | {Name} | {Category} | {Difficulty} ");
+        }
+
+        public Topic() {
+            this.Id = NextId;
+            NextId = NextId + 17;
+        }
+            //this.Id = Id;
+            //this.Name = Name;
+            //this.Category = Category;
+            //this.Difficulty = Difficulty;
+    }
+}
